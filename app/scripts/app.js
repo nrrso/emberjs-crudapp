b803af0bfe5a0ca2ad5adf985e1a2e32d07d0d00
Norris Sam Osarenkhoe <nrrsso@gmail.com>
2015-01-06 10:09:30 +0100

    minor tweaks

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,6 @@
-var EmberCrud = window.EmberCrud = Ember.Application.create();
+var EmberCrud = window.EmberCrud = Ember.Application.create({
+  LOG_TRANSITIONS: true
+});
 
 /* Order and include as you please. */
 require('scripts/controllers/*');
